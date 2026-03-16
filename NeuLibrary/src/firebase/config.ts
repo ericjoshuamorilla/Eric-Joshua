924d57e2944b9cf54b35221ce14bed9fd3f5298a
Eric Joshua Morilla <ericjoshua.morilla@neu.edu.ph>
2026-03-16 18:39:09 +0800

    Update config.ts

--- NeuLibrary/src/firebase/config.ts
+++ NeuLibrary/src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1241376197-e6b69",
   "appId": "1:285569549634:web:ccd104c941e3ff109e74db",
-  "apiKey": "AIzaSyDYeTt-h8rXbcYorFKYbZklfh1IU3Av1l0",
+  
   "authDomain": "studio-1241376197-e6b69.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "285569549634"
