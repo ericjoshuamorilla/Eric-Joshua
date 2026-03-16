7884b5c6a0478cd755ecdc6fb8db93348daf9cc1
Eric Joshua Morilla <ericjoshua.morilla@neu.edu.ph>
2026-03-16 18:41:46 +0800

    Delete NeuLibrary/src directory

--- NeuLibrary/src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-1241376197-e6b69",
-  "appId": "1:285569549634:web:ccd104c941e3ff109e74db",
-  
-  "authDomain": "studio-1241376197-e6b69.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "285569549634"
-};
